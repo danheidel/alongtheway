bb045c3a8adb3a37658d3ec3ceeda7532f952268
Dan Heidel <dan.heidel@gmail.com>
2014-02-26 15:18:48 -0800

    refactored googlemaps.js

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,10 +1,20 @@
-(function(){
-  window.googleMaps = window.googleMaps || (window.googleMaps = {});
-  googleMaps.map = {};
-  googleMaps.infoWindow = {};
-  googleMaps.placesService = {};
-  googleMaps.routeBoxer = {};
-  googleMaps.directionRenderer = {};
-  googleMaps.directionsService = {};
-  googleMaps.directionsDisplay = {};
-})();
+(function(NS){
+
+  NS.initialize = function(lat, long, zoom, type){
+    var mapOptions = {
+      center: new google.maps.LatLng(lat || 47.624, lng || 237.664),
+      mapTypeId: type || google.maps.MapTypeId.ROADMAP,
+      zoom: zoom || 12
+    };
+    NS.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    NS.directionsDisplay = new google.maps.DirectionsRenderer();
+    NS.directionsDisplay.setPanel(document.getElementById('directions-panel'));
+    NS.directionsDisplay.setMap(map);
+    NS.directionsService = new google.maps.DirectionsService();
+    NS.directionRenderer = new google.maps.DirectionsRenderer({ map: map });
+    NS.placesService = new google.maps.places.PlacesService(map);
+    NS.routeBoxer = new RouteBoxer();
+    NS.infoWindow = new google.maps.InfoWindow();
+  };
+
+})(window.googleMaps = window.googleMaps || {});
