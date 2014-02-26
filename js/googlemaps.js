0eacfd893a0442ae0cf8548bf199269f5199b7ff
Dan Heidel <dan.heidel@gmail.com>
2014-02-26 14:33:07 -0800

    added googlemaps.js

---
+++ js/googlemaps.js
@@ -0,0 +1,10 @@
+(function(){
+  window.googleMaps = window.googleMaps || (window.googleMaps = {});
+  googleMaps.map = {};
+  googleMaps.infoWindow = {};
+  googleMaps.placesService = {};
+  googleMaps.routeBoxer = {};
+  googleMaps.directionRenderer = {};
+  googleMaps.directionsService = {};
+  googleMaps.directionsDisplay = {};
+})();
