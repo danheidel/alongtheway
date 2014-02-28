5225b97e138588453d333843f59607b559cc5ccf
Dan Heidel <dan.heidel@gmail.com>
2014-02-27 20:29:52 -0800

    small edit to query.js

--- js/query.js
+++ js/query.js
@@ -1,3 +1,13 @@
 (function(NS){
+  NS.PointToPoint = function(query, latLngArray){
 
+  };
+
+  NS.PointPlusDelta = function(query, latLngArray){
+
+  };
+
+  NS.PointCenteredDelta = function(query, latLngArray){
+
+  };
 })(window.query = window.query || {});
