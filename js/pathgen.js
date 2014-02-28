4a683662c79aa731252748c7107d5561639542bc
Dan Heidel <dan.heidel@gmail.com>
2014-02-27 19:10:25 -0800

    added time and distance to output of pathgen

--- js/pathgen.js
+++ js/pathgen.js
@@ -1,10 +1,10 @@
 (function(NS){
-  NS.calcRoute = function(callback, start, end, iPointDist, mode) {
+  NS.calcRoute = function(callback, start, end, iPointSpacing, mode) {
     var map = window.googleMaps.map;
     var directionsRenderer = window.googleMaps.directionsRenderer;
     var directionsService = window.googleMaps.directionsService;
     var infoWindow = window.googleMaps.infoWindow;
-    var pointDist = iPointDist || 10000;
+    var pointSpacing = iPointSpacing || 10000;
     var request = {
       origin: start,
       destination: end,
@@ -14,30 +14,77 @@
       if(status == google.maps.DirectionsStatus.OK){
         directionsRenderer.setDirections(result);
         var routeInfo = result.routes[0].legs[0];
-        showSteps(callback, routeInfo, pointDist);
+        showSteps(callback, routeInfo, pointSpacing);
+      }else{
+        callback('something went wrong with the google maps search');
       }
     });
   };
 
-  function showSteps(callback, routeInfo, pointDist){
+  function showSteps(callback, routeInfo, pointSpacing){
     //flush old data
-    var rep, rep2, stepPath;
+    var rep, rep2, stepPath, stepDist, stepTime;
     var compDist = google.maps.geometry.spherical.computeDistanceBetween;
     var LatLng = google.maps.LatLng;
+    var tempPointArray, tempTotalDelta, tempFracDelta, tempAdditiveTime, tempAdditiveDist;
     var savePoints = [];
 
     //add new data
     for(rep=0;rep<routeInfo.steps.length;rep++){
       //populate the list of lat/long
       stepPath = routeInfo.steps[rep].path;
-      savePoints.push(new LatLng(stepPath[0].d, stepPath[0].e));  //push first element
-      for(rep2=1;rep2<stepPath.length;rep2++){
-        var tempPoint = new LatLng(stepPath[rep2].d, stepPath[rep2].e);
-        //only add path points if at least pointDist away from the last pushed point
-        if(compDist(tempPoint, savePoints[savePoints.length - 1]) > pointDist){
-          savePoints.push(tempPoint);
+      stepDist = routeInfo.steps[rep].distance.value; //distance in m
+      stepTime = routeInfo.steps[rep].duration.value; //time in seconds
+      tempPointArray = [];
+      tempTotalDelta = 0;
+      //make a temp array of all path points in the step with deltas
+      //skip last point as it overlaps w/ first path point of next step
+      for(rep2 = 0;rep2 < (stepPath.length - 1);rep2++){
+        tempPointArray.push(stepPath[rep2]);
+        tempPointArray[rep2].isStep = (rep2 === 0) ? true : false;
+        tempPointArray[rep2].info = (rep2 === 0) ? routeInfo.steps[rep].instructions : '';
+        if(rep2 > 0){
+          //delta to next point
+          tempTotalDelta +=
+            tempPointArray[rep2 - 1].tempDelta =
+            compDist(tempPointArray[rep2], tempPointArray[rep2 - 1]);
         }
       }
+      //grab residual dist from 2nd to last point to actual step end
+      tempTotalDelta +=
+        tempPointArray[tempPointArray.length - 1].tempDelta =
+        compDist(tempPointArray[tempPointArray.length - 1], routeInfo.steps[rep].end_point);
+
+      //calculate delta fraction and fractional time and distance for each point
+      for(rep2 = 0;rep2 < tempPointArray.length;rep2 ++){
+        tempFracDelta = tempPointArray[rep2].tempDelta / tempTotalDelta;
+        tempPointArray[rep2].pointDist = stepDist * tempFracDelta;
+        tempPointArray[rep2].pointTime = stepTime * tempFracDelta;
+      }
+      // console.log('step length: ' + routeInfo.steps[rep].distance.value);
+      // console.log('total step delta: ' + tempTotalDelta);
+      // console.log((routeInfo.steps[rep].distance.value - tempTotalDelta) / tempTotalDelta * 100);
+
+      savePoints.push(tempPointArray[0]);  //push first element
+      tempAdditiveDist = tempPointArray[0].pointDist;
+      tempAdditiveTime = tempPointArray[0].pointTime;
+      for(rep2 = 1;rep2 < tempPointArray.length;rep2 ++){
+        tempAdditiveDist += tempPointArray[rep2].pointDist;
+        tempAdditiveTime += tempPointArray[rep2].pointTime;
+        //if the accumulated point distance from last saved point is > pointSpacing, save a point
+        if(tempAdditiveDist > pointSpacing){
+          tempPointArray[rep2].pointDist = tempAdditiveDist;
+          tempPointArray[rep2].pointTime = tempAdditiveTime;
+          savePoints.push(tempPointArray[rep2]);
+          tempAdditiveDist = 0;
+          tempAdditiveTime = 0;
+        }
+      }
+    }
+    //run through the entire list of saved points and make their time and distance values additive from the route start
+    for(rep = 1;rep < savePoints.length;rep ++){
+      savePoints[rep].pointDist += savePoints[rep - 1].pointDist;
+      savePoints[rep].pointTime += savePoints[rep - 1].pointTime;
     }
     callback(savePoints);
     // console.log(savePoints);
